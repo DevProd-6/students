6d6668be8067b68e7dc6a683944608eaa0c39374
devprod <devmoussac93@gmail.com>
2022-01-18 22:51:47 +0100

    add populateData method

--- src/main/java/com/students/students/DbUtils.java
+++ src/main/java/com/students/students/DbUtils.java
@@ -1,12 +1,23 @@
 package com.students.students;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DbUtils {
+    private final List<String> columnNames = new ArrayList<>();
     private Connection con;
     
     private String connect () {
@@ -43,5 +54,41 @@ public class DbUtils {
             }
         }
         return "Execution Successful";
+    }
+    
+    void populateData (TableView tv, String query) throws SQLException {
+        if (!tv.getColumns().isEmpty()) tv.getColumns().removeAll(tv.getColumns());
+        ResultSet resultSet = runQuery(query);
+        ObservableList<ObservableList<String>> data = FXCollections.observableArrayList();
+        
+        for (int i = 0; i < resultSet.getMetaData().getColumnCount(); i++) {
+            
+            final int j = i;
+            TableColumn col = new TableColumn(resultSet.getMetaData().getColumnName(i + 1));
+            col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> {
+                if (param.getValue().get(j) != null) {
+                    return new SimpleStringProperty(param.getValue().get(j).toString());
+                } else {
+                    return null;
+                }
+            });
+            
+            tv.getColumns().addAll(col);
+            this.columnNames.add(col.getText());
+        }
+        
+        while (resultSet.next()) {
+            //Iterate Row
+            ObservableList<String> row = FXCollections.observableArrayList();
+            for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
+                //Iterate Column
+                row.add(resultSet.getString(i));
+            }
+            data.add(row);
+            
+        }
+        
+        //FINALLY ADDED TO TableView
+        tv.setItems(data);
     }
 }
