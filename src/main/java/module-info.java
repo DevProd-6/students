a058b924ca959ce545c4d231ea2121b8bdc77aa6
devprod <devmoussac93@gmail.com>
2022-01-27 00:16:17 +0100

    updated module-info for PDF editing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.students.students {
     requires javafx.fxml;
     requires java.sql;
     requires org.mariadb.jdbc;
+    requires com.github.librepdf.openpdf;
+    requires java.desktop;
     
     
     opens com.students.students to javafx.fxml;
