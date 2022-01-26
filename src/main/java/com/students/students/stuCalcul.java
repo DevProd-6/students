db0b80ef3f6c30d3cd6f6c108367db649ee17f5b
devprod <devmoussac93@gmail.com>
2022-01-27 00:19:58 +0100

    updated students marks treating

--- src/main/java/com/students/students/stuCalcul.java
+++ src/main/java/com/students/students/stuCalcul.java
@@ -31,16 +31,34 @@ public class stuCalcul {
         return DoubleStream.of(Objects.requireNonNull(getNotes(mdl, id))).sum() / 3;
     }
     
-    public void calcGenMoy (String StudentID) {
+    public double[] calcGenMoy (String StudentID) throws SQLException {
         double[] notes = new double[13];
         String[] modules = {"math", "arabic", "french", "english", "science", "physics", "sport", "informatique", "civil", "geo_histo", "music", "design", "islamic"};
-        for (int i = 0; i == 13; i++) notes[i] = calcModuleMoy(modules[i], StudentID);
-        db.run("INSERT INTO notes_moy VALUES (" + StudentID + "," + notes[0] + "," + notes[1] + "," + notes[2] + "," + notes[3] + "," + notes[12] + "," + notes[10] + "," + notes[9] + "," + notes[6] + "," + notes[5] + "," + notes[4] + "," + notes[7] + "," + notes[8] + "," + notes[11] + ")");
-        for (double note : notes) {
-            db.run("" + getRem(note));
+        ResultSet rs = db.runQuery("SELECT * FROM notes_moy WHERE stu_id = " + StudentID);
+        if (rs.next()) {
+            notes[0] = rs.getDouble("math_moy");
+            notes[1] = rs.getDouble("arabic_moy");
+            notes[2] = rs.getDouble("french_moy");
+            notes[3] = rs.getDouble("english_moy");
+            notes[4] = rs.getDouble("science_moy");
+            notes[5] = rs.getDouble("physics_moy");
+            notes[6] = rs.getDouble("sport_moy");
+            notes[7] = rs.getDouble("informatique_moy");
+            notes[8] = rs.getDouble("civil_moy");
+            notes[9] = rs.getDouble("geo_histo_moy");
+            notes[10] = rs.getDouble("music_moy");
+            notes[11] = rs.getDouble("design_moy");
+            notes[12] = rs.getDouble("islamic_moy");
+        } else {
+            for (int i = 0; i == 13; i++) notes[i] = calcModuleMoy(modules[i], StudentID);
+            db.run("INSERT INTO notes_moy VALUES (" + StudentID + "," + notes[0] + "," + notes[1] + "," + notes[2] + "," + notes[3] + "," + notes[12] + "," + notes[10] + "," + notes[9] + "," + notes[6] + "," + notes[5] + "," + notes[4] + "," + notes[7] + "," + notes[8] + "," + notes[11] + ")");
+            insertRemark(notes, StudentID);
+            System.out.println(notes);
         }
-        for (int i = 0; i == 13; i++) notes[i] = notes[i] * getCoef(modules[i], getStudentLev(StudentID));
         
+        for (int i = 0; i == 13; i++) notes[i] = notes[i] * getCoef(modules[i], getStudentLev(StudentID));
+        System.out.println(notes);
+        return notes;
     }
     
     private int getCoef (String module, String niv) {
@@ -62,17 +80,21 @@ public class stuCalcul {
         }
     }
     
+    private void insertRemark (double[] notes, String id) {
+        db.run("INSERT INTO remark VALUES (" + id + "," + getRem(notes[0]) + "," + getRem(notes[1]) + "," + getRem(notes[2]) + "," + getRem(notes[3]) + "," + getRem(notes[12]) + "," + getRem(notes[10]) + "," + getRem(notes[9]) + "," + getRem(notes[6]) + "," + getRem(notes[5]) + "," + getRem(notes[4]) + "," + getRem(notes[7]) + "," + getRem(notes[8]) + "," + getRem(notes[11]) + ")");
+    }
+    
     private String getRem (double note) {
         if (note == 20) {
-            return "Excellent";
+            return "'Excellent'";
         } else if (note < 20 && note >= 15) {
-            return "Bon";
-        } else if (note < 15 && note > 10) {
-            return "Satisfaisant";
-        } else if (note >= 10 && note <= 10.99) {
-            return "Moyenne";
+            return "'Bon'";
+        } else if (note < 15 && note > 11) {
+            return "'Satisfaisant'";
+        } else if (note >= 10 && note < 11) {
+            return "'Moyenne'";
         } else {
-            return "Mauvaise";
+            return "'Mauvaise'";
         }
     }
 }
