710f61fc14f81952cf5f4911ea79fb18f5af76ef
devprod <devmoussac93@gmail.com>
2022-01-28 03:25:00 +0100

    fixed bugs and get accurate calculation for student object,with debugging outputs

--- src/main/java/com/students/students/stuCalcul.java
+++ src/main/java/com/students/students/stuCalcul.java
@@ -10,61 +10,62 @@ public class stuCalcul {
     DbUtils db = new DbUtils();
     
     private double[] getNotes (String mdl, String id) {
+        System.out.println("getNotes started...");
         double[] notes = new double[3];
-        String module = mdl;
-        ResultSet rs = db.runQuery("SELECT " + module.concat("_cc," + module.concat("_dv," + module.concat("_exmn"))) + " FROM notes_cc,notes_dv,notes_exmn WHERE notes_cc.stu_id = " + id + " and notes_dv.stu_id = " + id + "and notes_exmn"
-         + ".stu_id = " + id);
+        ResultSet rs = db.runQuery("SELECT " + mdl.concat("_cc," + mdl.concat("_dv," + mdl.concat("_ex"))) + " FROM notes_cc,notes_dv,notes_exmn WHERE notes_cc.stu_id = " + id + " and notes_dv.stu_id = " + id + " and notes_exmn" +
+         ".stu_id = " + id);
         try {
             if (rs.next()) {
                 notes[0] = rs.getDouble(mdl + "_cc");
                 notes[1] = rs.getDouble(mdl + "_dv");
-                notes[2] = rs.getDouble(mdl + "_exmn");
+                notes[2] = rs.getDouble(mdl + "_ex");
             }
+            System.out.println("getNotes finished...");
+            db.close();
             return notes;
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            db.close();
             return null;
         }
     }
     
     private double calcModuleMoy (String mdl, String id) {
+        System.out.println("calcModuleMoy started...");
+        System.out.println("calcModuleMoy finished...");
         return DoubleStream.of(Objects.requireNonNull(getNotes(mdl, id))).sum() / 3;
     }
     
     public double[] calcGenMoy (String StudentID) throws SQLException {
         double[] notes = new double[13];
-        String[] modules = {"math", "arabic", "french", "english", "science", "physics", "sport", "informatique", "civil", "geo_histo", "music", "design", "islamic"};
+        String[] modules = {"math", "arabic", "french", "english", "islamic", "civil", "geo_histo", "sport", "physics", "science", "informatique", "music", "design"};
         ResultSet rs = db.runQuery("SELECT * FROM notes_moy WHERE stu_id = " + StudentID);
         if (rs.next()) {
-            notes[0] = rs.getDouble("math_moy");
-            notes[1] = rs.getDouble("arabic_moy");
-            notes[2] = rs.getDouble("french_moy");
-            notes[3] = rs.getDouble("english_moy");
-            notes[4] = rs.getDouble("science_moy");
-            notes[5] = rs.getDouble("physics_moy");
-            notes[6] = rs.getDouble("sport_moy");
-            notes[7] = rs.getDouble("informatique_moy");
-            notes[8] = rs.getDouble("civil_moy");
-            notes[9] = rs.getDouble("geo_histo_moy");
-            notes[10] = rs.getDouble("music_moy");
-            notes[11] = rs.getDouble("design_moy");
-            notes[12] = rs.getDouble("islamic_moy");
+            for (int i = 0; i < 13; i++) {
+                System.out.println("iteration " + i);
+                notes[i] = calcModuleMoy(modules[i], StudentID);
+            }
+            db.close();
+            db.run("DELETE FROM notes_moy WHERE stu_id = " + StudentID);
         } else {
-            for (int i = 0; i == 13; i++) notes[i] = calcModuleMoy(modules[i], StudentID);
-            db.run("INSERT INTO notes_moy VALUES (" + StudentID + "," + notes[0] + "," + notes[1] + "," + notes[2] + "," + notes[3] + "," + notes[12] + "," + notes[10] + "," + notes[9] + "," + notes[6] + "," + notes[5] + "," + notes[4] + "," + notes[7] + "," + notes[8] + "," + notes[11] + ")");
-            insertRemark(notes, StudentID);
-            System.out.println(notes);
+            for (int i = 0; i < 13; i++) {
+                System.out.println("iteration " + i);
+                notes[i] = calcModuleMoy(modules[i], StudentID);
+            }
+            db.close();
         }
-        
-        for (int i = 0; i == 13; i++) notes[i] = notes[i] * getCoef(modules[i], getStudentLev(StudentID));
-        System.out.println(notes);
+        db.run("INSERT INTO notes_moy VALUES (" + StudentID + "," + notes[0] + "," + notes[1] + "," + notes[2] + "," + notes[3] + "," + notes[4] + "," + notes[5] + "," + notes[6] + "," + notes[7] + "," + notes[8] + "," + notes[9] + "," + notes[10] + "," + notes[11] + "," + notes[12] + ")");
+        insertRemark(notes, StudentID);
+        for (int i = 0; i < 13; i++) notes[i] = notes[i] * getCoef(modules[i], getStudentLev(StudentID));
         return notes;
     }
     
     private int getCoef (String module, String niv) {
         try {
             ResultSet rs = db.runQuery("SELECT coef FROM coef WHERE modul = '" + module + "' and niv = " + niv);
-            return (rs.next() ? rs.getInt("coef") : 0);
+            int a = (rs.next() ? rs.getInt("coef") : 0);
+            db.close();
+            return a;
         } catch (SQLException e) {
             return 0;
         }
@@ -74,13 +75,16 @@ public class stuCalcul {
     private String getStudentLev (String id) {
         try {
             ResultSet rs = db.runQuery("SELECT niv_scho FROM stu_class WHERE stu_id = " + id);
-            return "" + (rs.next() ? rs.getInt("niv_scho") : 1);
+            int a = (rs.next() ? rs.getInt("niv_scho") : 1);
+            db.close();
+            return "" + a;
         } catch (SQLException e) {
             return "1";
         }
     }
     
     private void insertRemark (double[] notes, String id) {
+        db.run("DELETE FROM remark WHERE stu_id = " + id);
         db.run("INSERT INTO remark VALUES (" + id + "," + getRem(notes[0]) + "," + getRem(notes[1]) + "," + getRem(notes[2]) + "," + getRem(notes[3]) + "," + getRem(notes[12]) + "," + getRem(notes[10]) + "," + getRem(notes[9]) + "," + getRem(notes[6]) + "," + getRem(notes[5]) + "," + getRem(notes[4]) + "," + getRem(notes[7]) + "," + getRem(notes[8]) + "," + getRem(notes[11]) + ")");
     }
     
