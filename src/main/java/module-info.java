fb1fb0db3a05074dec33f239eb4896449ddf87d7
devprod <devmoussac93@gmail.com>
2022-01-30 04:17:09 +0100

    added stylesheet

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,8 @@ module com.students.students {
     requires org.mariadb.jdbc;
     requires com.github.librepdf.openpdf;
     requires java.desktop;
-    
+    requires transitive javafx.web;
+    requires transitive javafx.swing;
     
     opens com.students.students to javafx.fxml;
     exports com.students.students;
